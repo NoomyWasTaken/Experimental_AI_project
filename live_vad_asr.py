991d4e6329a1622ac1da62310f3448e6094ddc70
tljk <tanglinchuanz@163.com>
2024-05-08 17:50:16 -0400

    Revert "UI is made, every emotion has different colors[C"
    This reverts commit a7afbf5330cc6aa5cc320b9e5810588c5c13d00e.

--- live_vad_asr.py
+++ live_vad_asr.py
@@ -12,11 +12,6 @@ from rx.subject import BehaviorSubject
 import time
 from sys import exit
 
-import os
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
-
-DEFAULT_SAMPLE_RATE = 16000
-
 class Audio(object):
     """Streams raw audio from microphone. Data is received in a separate thread, and stored in a buffer, to be read from."""
 
@@ -84,7 +79,7 @@ class VADAudio(Audio):
         """Generator that yields all audio frames from microphone."""
         if self.input_rate == self.RATE_PROCESS:
             while True:
-                    yield self.read()
+                yield self.read()
         else:
             raise Exception("Resampling required")
 
@@ -125,16 +120,17 @@ class VADAudio(Audio):
                     yield None
                     ring_buffer.clear()
 
-def main(ARGS,stop_event,queue):
+
+def main(ARGS):
     model_name = "jonatasgrosman/wav2vec2-large-english"
 
     wave_buffer = BehaviorSubject(np.array([]))
     wave2vec_asr = WhisperInference(model_name)
     wave2vec_ser = WavLMInferenceSER(model_name)
     wave_buffer.subscribe(
-        on_next=lambda x: asr_output_formatter(wave2vec_asr, x, queue))
+        on_next=lambda x: asr_output_formatter(wave2vec_asr, x))
     wave_buffer.subscribe(
-        on_next=lambda x: ser_output_formatter(wave2vec_ser, x, queue))
+        on_next=lambda x: ser_output_formatter(wave2vec_ser, x))
 
     # Start audio with VAD
     vad_audio = VADAudio(aggressiveness=ARGS.webRTC_aggressiveness,
@@ -161,8 +157,6 @@ def main(ARGS,stop_event,queue):
     wav_data = bytearray()
     try:
         for frame in frames:
-            if stop_event.is_set(): # if receive stop event, exit
-                exit()
             if frame is not None:
                 if spinner:
                     spinner.start()
@@ -182,28 +176,24 @@ def main(ARGS,stop_event,queue):
                     wave_buffer.on_next(audio_float32.numpy())
                 else:
                     print("VAD detected noise")
-                    queue.put("VAD detected noise")
                 wav_data = bytearray()
     except KeyboardInterrupt:        
         exit()
 
-def asr_output_formatter(asr, audio, queue):
+
+def asr_output_formatter(asr, audio):
     start = time.perf_counter()
     text = asr.buffer_to_text(audio)
     inference_time = time.perf_counter()-start
     sample_length = len(audio) / DEFAULT_SAMPLE_RATE
-    print(f"{sample_length:.3f}s\t{inference_time:.3f}s\t{text}")
-    message = f"text: {text}"
-    queue.put(message)
+    print(f"{sample_length:.3f}s\t{inference_time:.3f}s\t{text}")    
 
-def ser_output_formatter(ser, audio, queue):
+def ser_output_formatter(ser, audio):
     start = time.perf_counter()
     text = ser.buffer_to_text(audio)
     inference_time = time.perf_counter()-start
     sample_length = len(audio) / DEFAULT_SAMPLE_RATE
     print(f"{sample_length:.3f}s\t{inference_time:.3f}s\t{text}")
-    message = f"emotion: {text}"
-    queue.put(message)
 
 def Int2FloatSimple(sound):
     return torch.from_numpy(np.frombuffer(sound, dtype=np.int16).astype('float32') / 32767)
@@ -220,6 +210,7 @@ def Int2Float(sound):
 
 
 if __name__ == '__main__':
+    DEFAULT_SAMPLE_RATE = 16000
 
     import argparse
     parser = argparse.ArgumentParser(
